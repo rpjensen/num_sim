44d1066d631c9c4aa5c9e15506812787fcf75ccc
Ryan Jensen <rjensen.professional@gmail.com>
2015-09-28 20:10:12 -0400

    Rough draft of rk12.  Starting debugging process now.

--- jensen_ns_midterm/problem3.m
+++
@@ -1,9 +0,0 @@
-function [ dy ] = problem3( t, y )
-% RHS Function for hw 3 problem 3
-
-dy = zeros(2,1);
-dy(1) = 2*y(1) - 1.2*y(1).*y(2);
-dy(2) = -y(2) + 0.9*y(1).*y(2);
-
-end
-
--- jensen_ns_midterm/rk12.m
+++ jensen_ns_midterm/rk12.m
@@ -1,6 +1,55 @@
-function [ output_args ] = rk12( input_args )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ T, Y, stepSizes ] = rk12( f, times, y0, TOL )
+
+LOW_TOL = 0.8*TOL;% the bound at which we increase h
+
+h = (times(end)-times(1))/20;% init step size
+t = times(1);% init t
+y = y0;% init y
+
+T = times;% output T
+Y = zeros(length(T), 1);% output Y (1 to 1 with T)
+stepSizes = zeros(2,1);% (ti,hi) pairs for each accepted step (dynmaic size)
+
+timeCounter = 2;% the index into the time array
+stepCounter = 1;% the index into the steps array
+
+while (t < times(end))
+    % h is the min of h and the distance to the next fence post
+    h = min(h, times(timeCounter)-t);
+    
+    % calculate fe and ie at this step
+    fe = forwardEuler(f,h,t,y);
+    ie = improvedEuler(f,h,t,y);
+    
+    % calculate iffy error
+    err = 1.5*abs(ie-fe);
+    
+    if (err > TOL)
+        % bad step reduce h for the next step
+       h = h/2;
+    else 
+        % good step record the time, stepsize pair in our steps matrix
+        stepSizes(stepCounter,:) = [t h];
+        stepCounter = stepCounter + 1;
+        
+        % update t and y for the next step
+        t = t+h;
+        y = ie;
+        
+        if t == times(timeCounter)
+           % need to record the value we found since this is at one of our
+           % fence posts
+           Y(timeCounter) = y;
+           timeCounter = timeCounter + 1;
+        end
+        
+        if h < LOW_TOL
+            % really good step, make h a little bigger
+            h = h * sqrt(TOL/err);
+        end
+    end
+    
+end
 
 
 end
